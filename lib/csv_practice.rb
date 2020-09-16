9da37957e9744bffd4ae476a26a1ecbd3c424747
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-16 08:31:33 -0700

    Implemented source code to pass first three tests (method: get_all_olympic_athletes)

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,8 +1,15 @@
 require 'csv'
 require 'awesome_print'
 
-def get_all_olympic_athletes(filename)
+REQUIRED_OLYMPIAN_FIELDS = %w[ID Name Height Team Year City Sport Event Medal]
 
+def get_all_olympic_athletes(filename)
+  # athletes = CSV.read('data/athlete_events.csv', headers: true).map { |row| row.to_h }
+  temp_athletes = CSV.read('data/athlete_events.csv', headers: true).map { |row| row.to_h }
+  athletes = temp_athletes.each do |athlete|
+    athlete.filter! { |key| REQUIRED_OLYMPIAN_FIELDS.include?(key) }
+  end
+  return athletes
 end
 
 def total_medals_per_team(olympic_data)
