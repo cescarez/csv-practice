56e1208c3f68180b73cec51908acf83abc6aa6c2
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-16 14:08:26 -0700

    Implemented total_medals_per_team

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,10 +1,9 @@
 require 'csv'
 require 'awesome_print'
 
-REQUIRED_OLYMPIAN_FIELDS = %w[ID Name Height Team Year City Sport Event Medal]
+
 
 def get_all_olympic_athletes(filename)
-  # athletes = CSV.read('data/athlete_events.csv', headers: true).map { |row| row.to_h }
   temp_athletes = CSV.read('data/athlete_events.csv', headers: true).map { |row| row.to_h }
   athletes = temp_athletes.each do |athlete|
     athlete.filter! { |key| REQUIRED_OLYMPIAN_FIELDS.include?(key) }
@@ -13,31 +12,31 @@ def get_all_olympic_athletes(filename)
 end
 
 def total_medals_per_team(olympic_data)
-
-
-
-  all_medals = olympic_data.map do |athlete|
-    if REQUIRED_OLYMPIAN_FIELDS[-1] != "NA"
-      {athlete[REQUIRED_OLYMPIAN_FIELDS[3]] => athlete[REQUIRED_OLYMPIAN_FIELDS[-1]]}
+  all_medals = []
+  olympic_data.each do |athlete|
+    if athlete[REQUIRED_OLYMPIAN_FIELDS[-1]] != "NA"
+      all_medals << {athlete[REQUIRED_OLYMPIAN_FIELDS[3]] => athlete[REQUIRED_OLYMPIAN_FIELDS[-1]]}
     else
       next
     end
   end
-  all_medals = all_medals.sort.to_h
-  ap all_medals
-  current_team = ''
-  team_medals = []
-  all_medals.each do |team|
-    if current_team == team
-      team_medals[team] += 1
-    else
-      current_team = team
-      team_medals << {team => 1}
+
+  # #sort is unnecessary
+  # all_medals.sort_by! { |medal_info| medal_info.keys.first  }
+
+  team_medals = {}
+
+  all_medals.each do |medal_info|
+    medal_info.each do |team, medal|
+      if team_medals.member?(team)
+        team_medals[team] += 1
+      else
+        team_medals[team] = 1
+      end
     end
   end
 
-  # team_medals = all_medals.sort_ do |team| {team => team.count
-  ap team_medals
+  return team_medals
 end
 
 def get_all_gold_medalists(olympic_data)
